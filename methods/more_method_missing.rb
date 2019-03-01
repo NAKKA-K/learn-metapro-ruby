d70fde285757d8922d23f92cbf94988b1feb0004
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-02 03:47:40 +0900

    Add more method missing

---
+++ methods/more_method_missing.rb
@@ -0,0 +1,14 @@
+class Lawyer
+  def method_missing(method, *args)
+    puts "呼び出した: #{method}(#{args.join(', ')})"
+    puts "(ブロックも渡した)" if block_given?
+  end
+end
+
+bob = Lawyer.new
+bob.talk_simple('a', 'b') do
+  # block
+end
+
+# => 呼び出した: talk_simple(a, b)
+# => (ブロックも渡した)
