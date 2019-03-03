bdcf423261f8a7402f746e5e5b9b08214124c033
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-04 00:31:47 +0900

    Add blank slate

---
+++ methods/computer/blank_slate.rb
@@ -0,0 +1,28 @@
+class DS
+  def get_cpu_info(id); "2.9 Ghz quad-core"; end
+  def get_cpu_price(id); 120; end
+  def get_mouse_info(id); "Wireless Touch"; end
+  def get_mouse_price(id); 60; end
+end
+
+class Computer < BasicObject
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def method_missing(name)
+    super if !@data_source.respond_to?("get_#{name}_info")
+    info = @data_source.send("get_#{name}_info", @id)
+    price = @data_source.send("get_#{name}_price", @id)
+    result = "#{name.capitalize}: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+    result
+  end
+end
+
+computer = Computer.new(0, DS.new)
+p computer.cpu
+p computer.mouse
+p computer.pc
+
