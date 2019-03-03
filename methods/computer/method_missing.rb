98d1d362e9e380cbd9fbe4bb7b4c66c7fb8c00fc
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-03 21:40:45 +0900

    Update info

--- methods/computer/method_missing.rb
+++ methods/computer/method_missing.rb
@@ -1,3 +1,10 @@
+class DS
+  def get_cpu_info(id); "2.9 Ghz quad-core"; end
+  def get_cpu_price(id); 120; end
+  def get_mouse_info(id); "Wireless Touch"; end
+  def get_mouse_price(id); 60; end
+end
+
 class Computer
   def initialize(computer_id, data_source)
     @id = computer_id
@@ -5,6 +12,8 @@ class Computer
   end
 
   def method_missing(name)
+    # 別インスタンスのmethod_missingを呼び出すこともできる。ユーザーが困惑するためやらないが。
+    # @data_source.send(:method_missing, name) if !@data_source.respond_to?("get_#{name}_info")
     super if !@data_source.respond_to?("get_#{name}_info")
     info = @data_source.send("get_#{name}_info", @id)
     price = @data_source.send("get_#{name}_price", @id)
@@ -13,3 +22,8 @@ class Computer
     result
   end
 end
+
+computer = Computer.new(0, DS.new)
+p computer.cpu
+p computer.mouse
+p computer.pc
