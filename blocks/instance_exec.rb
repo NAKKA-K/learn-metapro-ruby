7ccebb845dd7409e1b223010a482df2139ec1c11
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-06 02:34:23 +0900

    Add instance exec

--- blocks/instance_exec.rb
+++ blocks/instance_exec.rb
@@ -13,6 +13,15 @@ class D
     # ローカル変数はselfなどに関係しないため、Cのinstance_evalの中でも共有できる
     p C.new.instance_eval { "@x: #{@x}, @y: #{@y}, z: #{z}" }
   end
+
+  def twisted_method_with
+    @y = 2
+    z = 3
+
+    # instance_execは引数を渡せる
+    p C.new.instance_exec(@y) {|y| "@x: #{@x}, D::@y: #{y}, z: #{z}" }
+  end
 end
 
 D.new.twisted_method #=> "@x: 1, @y: , z: 3"
+D.new.twisted_method_with #=> "@x: 1, D::@y: 2, z: 3"
