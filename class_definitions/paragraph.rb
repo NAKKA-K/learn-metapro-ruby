7b639275742507c72a462f271b9fe221458cf7bd
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-08 05:18:35 +0900

    Update new paragraph

--- class_definitions/paragraph.rb
+++ class_definitions/paragraph.rb
@@ -1,9 +1,13 @@
-class Paragraph
-  def initialize(test)
-    @text = text
-  end
-
-  def title?; @text.upcase == @text; end
-  def reverse; @text.reverse; end
-  def upcase; @text.upcase; end
+def index(paragraph)
+  paragraph if paragraph.title?
 end
+
+
+paragraph ='any string can be a paragraph'
+
+# paragraphオブジェクトにのみこのメソッドを定義
+def paragraph.title?
+  self.upcase == self
+end
+
+p index(paragraph)
