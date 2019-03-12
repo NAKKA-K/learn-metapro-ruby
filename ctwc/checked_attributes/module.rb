9a7c23be6975867d92a0d51c7ea30123cf98a3c5
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-13 03:24:12 +0900

    Add the module of checked attributes

---
+++ ctwc/checked_attributes/module.rb
@@ -0,0 +1,57 @@
+require 'test/unit'
+
+module CheckedAttributes
+  # CheckedAttributesがincludeされた後、このメソッドが呼び出される。
+  # 通常includeだとインスタンスメソッドとしてのみ読み込まれる。
+  # しかしincludedメソッド内で別のmoduleをextendしてやることによってクラスメソッドも同時に定義することができる。
+  def self.included(klass)
+    klass.extend ClassMethods
+  end
+
+  module ClassMethods
+    def attr_checked(attribute, &validation)
+        define_method "#{attribute}=" do |value|
+          raise 'Invalid attribute' unless validation.call(value)
+          instance_variable_set "@#{attribute}", value
+        end
+
+        define_method attribute do
+          instance_variable_get "@#{attribute}"
+        end
+    end
+  end
+end
+
+class Person
+  include CheckedAttributes
+
+  attr_checked :age do |v|
+    v >= 18
+  end
+end
+
+
+class TestCheckedAttribute < Test::Unit::TestCase
+  def setup
+    @bob = Person.new
+  end
+
+  def test_accepts_valid_values
+    @bob.age = 20
+    assert_equal 20, @bob.age
+  end
+
+  def test_refuses_nil_values
+    assert_raises NoMethodError, 'Invalid attribute' do
+      @bob.age = nil
+    end
+  end
+
+  def test_refuses_false_values
+    assert_raises RuntimeError, 'Invalid attribute' do
+      @bob.age = 17
+    end
+  end
+end
+
+
