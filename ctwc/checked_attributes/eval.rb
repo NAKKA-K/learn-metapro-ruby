d1542b4bf9d220d80f8eced83a002e149e529e06
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-12 22:20:35 +0900

    Add checked attributes

--- ctwc/checked_attributes/eval.rb
+++ ctwc/checked_attributes/eval.rb
@@ -29,5 +29,16 @@ end
 
 # Todo ...
 def add_checked_attribute(klass, attribute)
-  # ...
+  eval "
+    class #{klass}
+      def #{attribute}=(value)
+        raise 'Invalid attribute' unless value
+        @#{attribute} = value
+      end
+
+      def #{attribute}()
+        @#{attribute}
+      end
+    end
+  "
 end
