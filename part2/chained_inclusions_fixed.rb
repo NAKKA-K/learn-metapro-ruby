42bf143b056e1097934433c8c37bf754234b27ce
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-14 03:23:15 +0900

    Fix

--- part2/chained_inclusions_fixed.rb
+++ part2/chained_inclusions_fixed.rb
@@ -1,24 +1,24 @@
 module SecondLevelModule
   def self.included(base)
-    base.extend ClassMethod
+    base.extend ClassMethods
   end
 
   def second_level_instance_method; 'ok'; end
 
-  module ClassMethod
+  module ClassMethods
     def second_level_class_method; 'ok'; end
   end
 end
 
 module FirstLevelModule
   def self.included(base)
-    base.extend ClassMethod
+    base.extend ClassMethods
     base.include SecondLevelModule
   end
 
   def first_level_instance_method; 'ok'; end
 
-  module ClassMethod
+  module ClassMethods
     def first_level_class_method; 'ok'; end
   end
 end
